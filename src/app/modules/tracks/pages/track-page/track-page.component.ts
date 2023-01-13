9739a6d777f6b841cd027d62b843b20a02895010
Marcela Malaver <marcela9409@gmail.com>
2023-01-13 14:59:46 -0500

    ✨ Add sections in tracks

--- src/app/modules/tracks/pages/track-page/track-page.component.ts
+++ src/app/modules/tracks/pages/track-page/track-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+// import { TrackService } from '@modules/tracks/services/track.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { TrackModel } from '@core/models/tracks.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-track-page',
   templateUrl: './track-page.component.html',
   styleUrls: ['./track-page.component.css']
 })
-export class TrackPageComponent {
+export class TrackPageComponent implements OnInit, OnDestroy {
+
+  tracksTrending: Array<any> = []
+  tracksRandom: Array<any> = []
+  listObservers$: Array<Subscription> = []
+
+  // constructor(private trackService: TrackService) { }
+
+  ngOnInit(): void {
+    this.loadDataAll() //TODO 📌📌
+    // this.loadDataRandom() //TODO 📌📌
+  }
+
+  async loadDataAll(): Promise<any> {
+    // this.tracksTrending = await this.trackService.getAllTracks$().toPromise()
+
+  }
+
+  // loadDataRandom(): void {
+  //   this.trackService.getAllRandom$()
+  //     .subscribe((response: TrackModel[]) => {
+  //       this.tracksRandom = response
+  //     })
+  // }
+
+  ngOnDestroy(): void {
+
+  }
 
 }
