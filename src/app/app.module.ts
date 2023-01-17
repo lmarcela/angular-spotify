f35e2696d92443e7454e0db9dc8fcd6f47f738b6
Marcela Malaver <marcela9409@gmail.com>
2023-01-17 09:02:57 -0500

    ✨ save cookie in component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,24 +1,30 @@
-// import { environment } from './../../../../environments/environment';
+import { environment } from './../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly URL = ''//environment.api
-  //constructor(private http: HttpClient) { }
+  private readonly URL = environment.api
+  constructor(private http: HttpClient, private cookie:CookieService) { }
 
-  sendCredentials(email: string, password: string): void//Observable<any> 
+  sendCredentials(email: string, password: string): Observable<any> 
   {
     const body = {
       email,
       password
     }
-    console.log(body)
-    //return this.http.post(`${this.URL}/auth/login`, body)
+    return this.http.post(`${this.URL}/auth/login`, body)
+    .pipe(
+      tap((responseOk:any)=>{
+        const {tokenSession,data} = responseOk
+        this.cookie.set('token_service',tokenSession,4,'/')
+      })
+    )
   }
 
   suma(a: number, b: number): number {
